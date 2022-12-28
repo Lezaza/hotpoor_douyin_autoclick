47616101d57adc6c4d5e9166500c38f65d2b91b5
Lezaza <Lezaza@qq.com>
2022-12-28 15:55:07 +0800

    多设备配置脚本

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hotpoor_douyin_autoclick
 hotpoor_douyin_autoclick
+
+# adb -s 5PBEL75H9XQOEUO7 shell am broadcast -a ADB_INPUT_TEXT --es msg 兔子178
+# 兰花
--- douyin_app/send_more_like.py
+++ douyin_app/send_more_like.py
@@ -10,10 +10,12 @@ def adb_tap(x,y,sleep=0):
     os.system(cmd_str)
     time.sleep(sleep)
 
+uni_min = 0.025
+uni_max = 0.15
 
-send_times = 222
+send_times = 1000
 for i in range(0,send_times):
     print("第%s次发送"%i)
     x = int(random.random()*10)+303
     y = int(random.random()*10)+767
-    adb_tap(x,y,0)
+    adb_tap(x,y,random.uniform(uni_min,uni_max))
